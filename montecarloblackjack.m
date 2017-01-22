82033a74bc217c43352c923cba5cd8ad9e401190
King Crawford <crawboy@me.com>
2017-01-22 07:08:55 -0500

    expanded convergence loop to contain initializations

--- montecarloblackjack.m
+++ montecarloblackjack.m
@@ -2,65 +2,69 @@ clear; clc; close all;
 
 %Optimal policy for blackjack using monte carlo
 
-deck = 4 * ones(13, 1);
-
-%dealer draws two cards
-[dealers_cards, deck] = draw(deck, 2);
-dealers_cards = check_for_aces(dealers_cards);
-dealer_faceup = dealers_cards(1)
-
-%player receives his own two cards
-[players_cards, deck] = draw(deck, 2);
-players_cards = check_for_aces(players_cards)
-
-neither_have_busted = true;
-player_busted = false;
-dealer_busted = false;
-
-while converging
-if sum(dealers_cards) == 21 || sum(players_cards) == 21
-    if sum(players_cards) == 21
-        fprintf('It was a draw')
-    continue
-    end
-
-players_turn = true;
-while players_turn && neither_have_busted
-    if enticed(dealer_faceup, players_cards)
-        [new_card, deck] = hit(deck);
-        players_cards = check_for_ace([players_cards; new_card]);
-        
-        if sum(players_cards) > 21
-            neither_have_busted = false;
-            player_busted = true;
-            fprintf('You busted.\n')
+while converging(condition)
+    deck = 4 * ones(13, 1);
+    
+    %dealer draws two cards
+    [dealers_cards, deck] = draw(deck, 2);
+    dealers_cards = check_for_aces(dealers_cards);
+    dealer_faceup = dealers_cards(1)
+    
+    %player receives his own two cards
+    [players_cards, deck] = draw(deck, 2);
+    players_cards = check_for_aces(players_cards)
+    
+    neither_have_busted = true;
+    player_busted = false;
+    dealer_busted = false;
+    
+    
+    if sum(dealers_cards) == 21 || sum(players_cards) == 21
+        if sum(players_cards) == 21
+            fprintf('It was a draw')
+            
+            % reward of 0
+            continue
+        else
+            
         end
-    else
-        players_turn = false;
-    end
-end
-
-dealers_turn = true;
-while dealers_turn && neither_have_busted
-    if sum(dealers_cards) <= sum(players_cards) && sum(dealers_cards ~= 21)
-        [new_card, deck] = hit(deck);
-        dealers_cards = [dealers_cards; new_card];
-        
-        if sum(dealers_cards) > 21
-            neither_have_busted = false;
-            dealer_busted = true;
-            fprintf('Dealer busts. You win\n')
+        players_turn = true;
+        while players_turn && neither_have_busted
+            if enticed(dealer_faceup, players_cards)
+                [new_card, deck] = hit(deck);
+                players_cards = check_for_ace([players_cards; new_card]);
+                
+                if sum(players_cards) > 21
+                    neither_have_busted = false;
+                    player_busted = true;
+                    fprintf('You busted.\n')
+                end
+            else
+                players_turn = false;
+            end
         end
-    else
-        fprintf('Dealer''s hand wins\n')
-        dealers_turn = false;
+        
+        dealers_turn = true;
+        while dealers_turn && neither_have_busted
+            if sum(dealers_cards) <= sum(players_cards) && sum(dealers_cards ~= 21)
+                [new_card, deck] = hit(deck);
+                dealers_cards = [dealers_cards; new_card];
+                
+                if sum(dealers_cards) > 21
+                    neither_have_busted = false;
+                    dealer_busted = true;
+                    fprintf('Dealer busts. You win\n')
+                end
+            else
+                fprintf('Dealer''s hand wins\n')
+                dealers_turn = false;
+            end
+        end
+        
     end
-end
-
-end
-fprintf('done\n')
-%for list of rules
-%http://wizardofodds.com/games/blackjack/basics/
-
-%to-do: overload sum function to account for aces, modify to accomodate
-%both human and ai players
+    fprintf('done\n')
+    %for list of rules
+    %http://wizardofodds.com/games/blackjack/basics/
+    
+    %to-do: overload sum function to account for aces, modify to accomodate
+    %both human and ai players
