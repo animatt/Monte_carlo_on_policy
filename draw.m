d69bff7686ee6e6c652ff35fcfb9a0ddc34948b6
King Crawford <crawboy@me.com>
2017-01-31 08:47:59 -0500

    fix bug in draw function

--- draw.m
+++ draw.m
@@ -6,11 +6,11 @@ for ii = 1:number_of_draws
     while ~found
         card_drawn = randi([1 13]);
         if deck(card_drawn) > 0
+            deck(card_drawn) = deck(card_drawn) - 1;
             if card_drawn > 10
                 card_drawn = 10;
             end
             cards = [cards; card_drawn];
-            deck(card_drawn) = deck(card_drawn) - 1;
             found = true;
         end
     end
