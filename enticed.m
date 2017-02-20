831fb916cba9b6f606e27c483e61c03420dc8ccb
King Crawford <crawboy@me.com>
2017-02-20 13:50:33 -0500

    preprocess matrix indices into policy for enticed function

--- enticed.m
+++ enticed.m
@@ -1,5 +1,5 @@
 function decide_to_hit = enticed(dealer, player, usable_ace, policy)
-if policy(player, dealer, usable_ace)
+if policy(player - 11, dealer - 1, usable_ace)
     decide_to_hit = true;
 else
     decide_to_hit = false;
